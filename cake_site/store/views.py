45ec9fca1f698342c7a7e98e536a0e9d6fbc4fe9
bishaldas1152006@gmail.com <bishaldas1152006@gmail.com>
2025-07-05 10:05:34 +0530

    .

--- cake_site/store/views.py
+++ cake_site/store/views.py
@@ -18,3 +18,13 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def search(request):
+    query = request.GET.get('q', '')
+    return render(request, 'search_cakes.html', {'query': query})
+
+def cakes(request):
+    return render(request, 'cakes.html')
+
+def categories(request):
+    return render(request, 'categories.html')
